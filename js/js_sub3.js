59f09f488431407f0edd703e9e32001e5d728640
Seri Lee <serirucy@gmail.com>
2025-12-16 13:55:47 +0900

    you

--- js/js_sub3.js
+++ js/js_sub3.js
@@ -2,3 +2,11 @@
 $('#tabBar-include').load('html/tabBar.html',function(){
   $('#tab-bar ul li:nth-child(3)').addClass('active');
 });
+//section shop
+$('section.shop .title span.close-btn').click(function(){
+  $('section.shop').hide();
+});
+//section profile - id
+$('section.profile h2 span.name img').click(function(){
+  $('section.profile .profile-name').toggleClass('close');
+});
